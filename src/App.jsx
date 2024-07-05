55fd05f35e0409cbab2cbb9fdae9da01a9fd5914
Hashimjasem <j_hashim1305@outlook.com>
2024-07-05 21:34:25 +0800

    scrapped

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
+import { Router, Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Register from './pages/Register';
+import Catagories from './pages/Catagories';
+import Navbar from './components/Navbar';
+
 
 function App() {
 
   return (
-    <div className='bg-black'>
-      <Navbar/>
-      
-    </div>
+    <>
+    <Navbar/>
+    <Router>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/catagories' element={<Catagories />} />
+        </Routes>
+    </Router>
+    </>
   )
 }
 
